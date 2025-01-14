293f49f7d3004ceb1d7c9dffb448678748ecef9a
Muhammad-Ilyas-Ibrahim <anonymous50137@gmail.com>
2025-01-14 14:52:13 +0500

    Added 13_Arrays.cpp - Practice file for arrays in C++

---
+++ CodeWithHarry_PracticeSet/13_Arrays.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	// 1st array and taking input and storing in Array
+	//  int ck1[10]={};
+	// cout<<" Enter 1st number :";cin>>ck1[1];
+	// cout<<ck1[1];
+
+	string ck[100] = {};
+	cout << " Enter the name: ";
+	cin >> ck[1];
+	cin >> ck[1];
+	cout << "\n Enter the age: ";
+	cin >> ck[3];
+	cout << "\n Enter the marks: ";
+	cin >> ck[4];
+	cout << " Dear " << ck[1] << " " << ck[1] << " your age is " << ck[3] << " and your marks are " << ck[4];
+	return 0;
+}
