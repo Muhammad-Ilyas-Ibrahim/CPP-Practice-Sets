88674f282a602200ec8237bf45f613d998e17f1b
Muhammad Ilyas <anonymous50137@gmail.com>
2025-04-03 00:36:26 +0500

    Added 70_STL_Containers.cpp - Practice file for STL containers in C++

---
+++ CodeWithHarry_PracticeSet/70_STL_Containers.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+/*
+        STL (Standard Template Library)
+
+        STL = Containers + Algorithms + Iterators
+        STL Componenets:
+        Containers:
+            Object which stores data
+        Algorithms:
+            Procedure which process data
+        Iterators:
+            Object which points to an element of Container
+        
+
+        Containers:
+        1-> Sequence Containers 
+            a-> Linear Fashion
+                (i)     Vector
+                (ii)    List
+                (iii)   Dequeue
+        2-> Associative Containers (Faster than Sequence Containers because of faster searching, deletion etc)
+            a-> Direct Access (Example : Tree)
+                (i)     Set / Multiset
+                (ii)    Maps / Multimaps
+        3-> Derived Containers
+            a-> Real World Modeling (Example: Stack, Queue, Priority Queue etc)
+
+
+        When to use which one?
+        Sequence Containers:
+            1-> Vector (Random access is fast but insertion and deletion is slow, But insertion at the end is fast)
+            2-> List (Random access is slow, Middle Insertion is fast, deletion and insertion at the end is fast)
+        Associative Containers:
+            1-> All Operation are fast except random access
+        Derived Containers:
+            Speed depends and need analysis because we are using Real World Modeling 
+
+*/
+int main(){
+
+return 0;
+}
